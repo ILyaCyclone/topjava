10cf8560224348ed627370a6c2717cc65d9cd8e3
ILya Cyclone <ilya.cyclone@gmail.com>
2018-10-12 22:44:23 +0300

    add Guava

--- src/main/java/ru/javawebinar/topjava/model/User.java
+++ src/main/java/ru/javawebinar/topjava/model/User.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.model;
 
+import com.google.common.collect.Sets;
+
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -22,8 +24,7 @@ public class User extends AbstractNamedEntity implements Comparable<User> {
     private int caloriesPerDay = DEFAULT_CALORIES_PER_DAY;
 
     public User(String name, String email, String password, Role... roles) {
-        //TODO use Guava for roles to set
-        this(null, name, email, password, DEFAULT_CALORIES_PER_DAY, true, Stream.of(roles).collect(Collectors.toSet()));
+        this(null, name, email, password, DEFAULT_CALORIES_PER_DAY, true, Sets.newHashSet(roles));
     }
     public User(Integer id, String name, String email, String password, Role role, Role... roles) {
         this(id, name, email, password, DEFAULT_CALORIES_PER_DAY, true, EnumSet.of(role, roles));
