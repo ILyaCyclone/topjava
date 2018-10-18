db3ad4e5cb1a799513e0d76c9efe66bd3cd8a3fa
ILya Cyclone <ilya.cyclone@gmail.com>
2018-10-19 01:14:21 +0300

    init database with meals table and populate its rows

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS meals;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START 100000;
@@ -16,6 +17,9 @@ CREATE TABLE users
 );
 CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
 
+
+
+
 CREATE TABLE user_roles
 (
   user_id INTEGER NOT NULL,
@@ -23,3 +27,17 @@ CREATE TABLE user_roles
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+
+
+
+CREATE TABLE meals
+(
+  id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  user_id          INTEGER                 NOT NULL,
+  date_time        TIMESTAMP DEFAULT now() NOT NULL,
+  description      VARCHAR                 NOT NULL,
+  calories         INTEGER                 NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+CREATE UNIQUE INDEX meals_unique_user_date_idx ON meals (user_id, date_time);
