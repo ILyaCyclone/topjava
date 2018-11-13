2070a7af222219bdf9ed61fbc0bd892375882c96
ILya Cyclone <ilya.cyclone@gmail.com>
2018-11-13 23:22:06 +0300

    add "Not found" tests

--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
@@ -7,6 +7,9 @@ import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.AbstractMealServiceTest;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 
 @ActiveProfiles(Profiles.DATAJPA)
 public class DataJpaMealServiceTest extends AbstractMealServiceTest {
@@ -14,5 +17,11 @@ public class DataJpaMealServiceTest extends AbstractMealServiceTest {
     public void getWithUser() {
         Meal meal = service.getWithUser(MealTestData.MEAL1_ID);
         UserTestData.assertMatch(meal.getUser(), UserTestData.USER);
+    }
+
+    @Test
+    public void getNotFound() throws Exception {
+        thrown.expect(NotFoundException.class);
+        service.getWithUser(ADMIN_ID);
     }
 }
--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
@@ -1,12 +1,15 @@
 package ru.javawebinar.topjava.service.datajpa;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.UserTestData;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.AbstractUserServiceTest;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import static ru.javawebinar.topjava.MealTestData.*;
 
@@ -19,6 +22,19 @@ public class DataJpaUserServiceTest extends AbstractUserServiceTest {
     public void getWithMeal() {
         User user = service.getWithMeal(UserTestData.USER_ID);
         MealTestData.assertMatch(user.getMeals(), MEAL6, MEAL5, MEAL4, MEAL3, MEAL2, MEAL1);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void getWithMealNotFound() throws Exception {
+        service.getWithMeal(1);
+    }
+
+    @Test
+    public void getWithMealEmpty() throws Exception {
+        User hungryUser = new User(null, "Hungry", "hunger@gmail.com", "password", Role.ROLE_USER);
+        User storedUser = service.create(hungryUser);
+        User storedUserWithMeal = service.getWithMeal(storedUser.getId());
+        Assertions.assertThat(storedUserWithMeal.getMeals().isEmpty()).withFailMessage("Hungry user meal should be empty");
     }
 
 }
